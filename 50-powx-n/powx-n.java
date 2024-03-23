a5705092be5826d3bc69e2727df77f533890beb4
Nawazish Bharwani <67267705+nawazish-bharwani@users.noreply.github.com>
2024-03-23 23:27:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (85.62%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,7 +1,7 @@
 class Solution {
     public double myPow(double x, int n) {
         double ans=1.0;
-        long pow=n;
+        long pow=n; //to handle overflow we took long as when me make max negative int positive it will overflow
         if(pow<0) {pow=-1*pow;}
         while(pow >0) {
             if(pow%2 == 0) {
